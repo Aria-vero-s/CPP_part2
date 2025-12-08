e1748161a0dc934282e59559c37411984d87756a
Ariane Saulnier <asaulnie@f0r7s11.clusters.42paris.fr>
2025-12-08 19:56:33 +0100

    with jacobsthal

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -1,6 +1,9 @@
 #include "PmergeMe.hpp"
 #include <algorithm>
 #include <utility>
+#include <vector>
+#include <deque>
+#include <cstddef>
 
 PmergeMe::PmergeMe() {}
 PmergeMe::~PmergeMe() {}
@@ -17,6 +20,43 @@ static void binaryInsertDeque(std::deque<int> &d, int value) {
 
 static bool pairCompare(const std::pair<int,int> &p1, const std::pair<int,int> &p2) {
 	return p1.second < p2.second;
+}
+
+static std::vector<std::size_t> jacobsthal_order(std::size_t m) {
+	std::vector<std::size_t> order;
+	if (m == 0)
+		return order;
+
+	std::vector<std::size_t> J;
+	J.push_back(0);
+	J.push_back(1);
+	while (true) {
+		std::size_t next = J[J.size() - 1] + (2 * J[J.size() - 2]);
+		if (next > m)
+			break;
+		J.push_back(next);
+	}
+
+	std::vector<char> used(m, false);
+	std::size_t k = 1;
+	while (k < J.size()) {
+		if (J[k] >= 1 && J[k] <= m) {
+			std::size_t idx = J[k] - 1;
+			if (!used[idx]) {
+				order.push_back(idx);
+				used[idx] = true;
+			}
+		}
+		++k;
+	}
+
+	std::size_t i = 0;
+	while (i < m) {
+		if (!used[i])
+			order.push_back(i);
+		++i;
+	}
+	return order;
 }
 
 std::vector<int> PmergeMe::sortVector(const std::vector<int> &input) {
@@ -57,11 +97,14 @@ std::vector<int> PmergeMe::sortVector(const std::vector<int> &input) {
 		mainChain.push_back(pairs[i].second);
 		++i;
 	}
-	i = 0;
-	while (i < pairs.size()) {
-		binaryInsertVector(mainChain, pairs[i].first);
-		++i;
+
+	std::vector<std::size_t> order = jacobsthal_order(pairs.size());
+	std::size_t idx = 0;
+	while (idx < order.size()) {
+		binaryInsertVector(mainChain, pairs[order[idx]].first);
+		++idx;
 	}
+
 	if (pending >= 0)
 		binaryInsertVector(mainChain, pending);
 	return mainChain;
@@ -97,11 +140,14 @@ std::deque<int> PmergeMe::sortDeque(const std::vector<int> &input) {
 		mainChain.push_back(pairs[i].second);
 		++i;
 	}
-	i = 0;
-	while (i < pairs.size()) {
-		binaryInsertDeque(mainChain, pairs[i].first);
-		++i;
+
+	std::vector<std::size_t> order = jacobsthal_order(pairs.size());
+	std::size_t j = 0;
+	while (j < order.size()) {
+		binaryInsertDeque(mainChain, pairs[order[j]].first);
+		++j;
 	}
+
 	if (pending >= 0)
 		binaryInsertDeque(mainChain, pending);
 	return mainChain;
